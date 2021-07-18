0f7634fd358eafa1c3eb2eacac828964c7b06fef
nurullah.demir <nurullah.demir@gvg.com.tr>
2021-07-18 17:05:01 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <Text>
+                Hello World
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+export default App;
