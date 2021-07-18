355812633442c86b855bddd32f098f4ea0b57084
nurullah.demir <nurullah.demir@gvg.com.tr>
2021-07-18 19:52:28 +0300

    Changed card overlay bg color.

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Chat from "../screens/Chat";
 import Login from "../screens/Login";
 import Onboard from "../screens/Onboard";
+import { View } from 'react-native';
 
 export type RootStackParamList = {
     OnboardScreen: undefined
@@ -22,6 +23,14 @@ export const RootNavigator = () => {
                 headerStyle: {
                     backgroundColor: '#AC3964',
                 },
+                headerTintColor: '#FFF',
+                cardOverlay: () => (
+                    <View
+                        style={{
+                            flex: 1,
+                            backgroundColor: 'rgba(93,70,144,.3)',
+                        }}
+                    />)
             }}
         >
             <MainStack.Screen name={"OnboardScreen"} component={Onboard} options={{ headerShown: false }} />
