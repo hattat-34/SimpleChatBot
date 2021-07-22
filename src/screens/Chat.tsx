b7bf45c0dd1b155283c4ba4f8d0cb5ddb2dc7811
nurullah.demir <nurullah.demir@gvg.com.tr>
2021-07-22 23:53:59 +0300

    Added aws amplify and dependencies.

--- src/screens/Chat.tsx
+++ src/screens/Chat.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Interactions } from 'aws-amplify';
+import { ChatBot } from 'aws-amplify-react-native'
 
 const Chat = () => {
+
+    let userInput = "Beam me up, Scotty!";
+
+    useEffect(() => {
+        (async () => {
+            // Provide a bot name and user input
+            const response = await Interactions.send("SpaceTrip", userInput);
+
+            // Log chatbot response
+            console.log(response.message);
+        })()
+    }, [])
+
+    const onComplete = () => {
+        console.log("onComplete")
+    }
+
     return (
-        null
+        <ChatBot
+            botName="SpaceTrip"
+            onComplete={onComplete}
+            conversationModeOn
+        />
     )
 }
 
